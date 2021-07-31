6860952284e7d92fbaf1a5b1aebd2dc30bbc9472
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-07-31 15:57:22 +0200

    fix test function IV

--- netlify/functions/bot-status.js
+++ netlify/functions/bot-status.js
@@ -1,22 +1,13 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
-	axios
-		.get(`https://discord.com/api/v9/users/@me`, {
-			headers: {
-				Authorization: `Bot ${process.env.token}`,
-			},
-		})
-		.then((response) => {
-			return {
-				statusCode: 200,
-				body: JSON.stringify(response.data),
-			};
-		})
-		.catch((error) => {
-			return {
-				statusCode: 500,
-				body: JSON.stringify({ errorMsg: 'discord api shat itself brb' }),
-			};
-		});
+	let response = await axios.get(`https://discord.com/api/v9/users/@me`, {
+		headers: {
+			Authorization: `Bot ${process.env.token}`,
+		},
+	});
+	return {
+		statusCode: 200,
+		body: JSON.stringify(response.data),
+	};
 };
