de3cf665c375bf1743a3b859c669070cfc332699
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-07-31 15:11:33 +0200

    add discord api test call

---
+++ netlify/functions/bot-status.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+exports.handler = async function(event, context) {
+	axios
+		.get(`https://discord.com/api/v9/users/@me`, {
+			headers: {
+				Authorization: `Bot ${process.env.token}`,
+			},
+		})
+		.then((response) => {
+			return {
+				statusCode: 200,
+				body: response.data,
+			};
+		})
+		.catch((error) => {
+			return {
+				statusCode: 500,
+				body: JSON.stringify('discord api shat itself brb'),
+			};
+		});
+};
