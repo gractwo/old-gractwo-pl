76f9db1b432ab21e0cccdf14fbe42043663c7510
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-08-01 16:11:02 +0200

    fix external link in cc

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -121,7 +121,7 @@
 			<h3>Gractwo 2020-2021</h3>
 			<p>
 				Napisane i hostowane przez
-				<g-link to="/jamesen-external">Jakuba Mańczaka</g-link>. Kod dostępny na
+				<g-link to="/jamesen-external" target="_blank">Jakuba Mańczaka</g-link>. Kod dostępny na
 				<a href="/github" target="_blank">GitHubie</a>.
 			</p>
 		</section>
