fdb5a57ff7938dfce476882f1d791e84a22d9532
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-08-01 13:25:38 +0200

    delete broken status response from function

--- netlify/functions/bot-status.js
+++ netlify/functions/bot-status.js
@@ -6,20 +6,10 @@ exports.handler = async function(event, context) {
 			Authorization: `Bot ${process.env.token}`,
 		},
 	});
-	let statusResponse = await axios.get(
-		`https://discord.com/api/v9/users/@me/connections`,
-		{
-			headers: {
-				Authorization: `Bot ${process.env.token}`,
-			},
-		}
-	);
-	console.log(statusResponse.data);
 	return {
 		statusCode: 200,
 		body: JSON.stringify({
 			info: response.data,
-			status: statusResponse.data,
 		}),
 		headers: {
 			'Content-Type': 'application/json',
